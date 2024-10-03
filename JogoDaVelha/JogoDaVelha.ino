6ad4942d4653e6da3d2cc0cccfc31c3e0b86dba6
DenadaiSenai <marcio.denadai@gmail.com>
2024-10-03 14:33:48 -0300

    JSON Format

--- JogoDaVelha/JogoDaVelha.ino
+++ JogoDaVelha/JogoDaVelha.ino
@@ -1,9 +1,20 @@
+
+#include <ArduinoJson.h>
+
+#define ARRAY_SIZE(array) ((sizeof(array)) / (sizeof(array[0])))
+
 // Declaração de variáveis
 int velha;
 int linha;
 int coluna;
 bool HaVencedor = false;
 int tabuleiro[9];
+int jogadorDaVez = 1;
+
+// Jogo - JSON para envio ao Node-red pela serial
+JsonDocument Jogo;
+
+// Armazena a jogada
 String jogada;
 
 // Função para zerar tabuleiro
@@ -33,48 +44,84 @@ bool validaJogada(String entrada) {
   return entradaValida;
 }
 
-void imprimeTabuleiro() {
-  Serial.println("-----");
+// void imprimeTabuleiro() {
+//   Serial.println("-----");
 
-  for (char l = 0; l < 3; l++) {
-    //for (char c = 0; c < 3; c++) {
-    // Imprime tabuleiro segunda linha
-    Serial.print(tabuleiro[l * 3 + 0]);
-    Serial.print("|");
-    Serial.print(tabuleiro[l * 3 + 1]);
-    Serial.print("|");
-    Serial.println(tabuleiro[l * 3 + 2]);
-    Serial.println("-----");
-    //}
+//   for (char l = 0; l < 3; l++) {
+//     //for (char c = 0; c < 3; c++) {
+//     // Imprime tabuleiro segunda linha
+//     Serial.print(tabuleiro[l * 3 + 0]);
+//     Serial.print("|");
+//     Serial.print(tabuleiro[l * 3 + 1]);
+//     Serial.print("|");
+//     Serial.println(tabuleiro[l * 3 + 2]);
+//     Serial.println("-----");
+//     //}
+//   }
+// }
+
+// void TabuleiroJSON() {
+//   String partida = "";
+//   for (char l = 0; l < 9; l++) {
+//     // partida = String(partida + tabuleiro[l]);
+//     partida += String(tabuleiro[l]);
+//   }
+
+//   Serial.println("{ tabuleiro: " + partida + "}");
+// }
+
+String IntArrayToString(int matriz[], int size) {
+  String result = "";
+  for (int i = 0; i < size; i++) {
+    result += String(matriz[i]);
   }
+  return result;
+}
+
+void PartidaJSON(String msg) {
+  Jogo["jogada"] = jogada;
+  Jogo["velha"] = velha;
+  Jogo["linha"] = linha;
+  Jogo["coluna"] = coluna;
+  Jogo["HaVencedor"] = HaVencedor;
+  Jogo["tabuleiro"] = IntArrayToString(tabuleiro, ARRAY_SIZE(tabuleiro));
+  Jogo["msg"] = msg;
+
+  serializeJson(Jogo, Serial);
+  Serial.println();
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  // Variáveis glocais que serão enviadas ao Node-red
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // Zera Tabuleiro
-  int jogadorDaVez = 1;
   zerarTabuleiro();
   // Inicia variáveis do jogo
-  bool HaVencedor = false;
+  HaVencedor = false;
   velha = 0;
 
+  PartidaJSON(F("Partida Iniciada"));  // Envia o JSON do estado da partida (Início)
   do {
+    // JSON Tabuleiro
+    //TabuleiroJSON();
     // Imprime o tabuleiro
-    imprimeTabuleiro();
+    //imprimeTabuleiro();
+    PartidaJSON("Digite sua jogada jogador " + String(jogadorDaVez));
 
     // Limpa a variável que armazena a jogada
     jogada = "";
     // Digitar a jogada
-    Serial.print("Digite sua jogada jogador ");
-    Serial.println(jogadorDaVez);
+    // Serial.print("Digite sua jogada jogador ");
+    // Serial.println(jogadorDaVez);
 
     // Espera o comando da serial
-    while (!Serial.available());
+    while (!Serial.available())
+      ;
     // Recebe a jogada pela serial
     jogada = Serial.readStringUntil('\n');
     //jogada = Serial.readString();
@@ -84,27 +131,24 @@ void loop() {
       // Serial.println("Se jogada válida continue .....");
       linha = int(jogada[0] - '0');
       coluna = int(jogada[2] - '0');
-      Serial.print("Linha:");
-      Serial.print(linha);
-      Serial.print(", Coluna:");
-      Serial.println(coluna);
+      // Serial.print("Linha:");
+      // Serial.print(linha);
+      // Serial.print(", Coluna:");
+      // Serial.println(coluna);
+
+
       if (tabuleiro[3 * linha + coluna] == 0) {
         // Se for verdade ..
         // Serial.println("Jogadar inserido ..");
         tabuleiro[3 * linha + coluna] = jogadorDaVez;
-        if ((tabuleiro[0] == jogadorDaVez && tabuleiro[3] == jogadorDaVez && tabuleiro[6] == jogadorDaVez) ||
-            (tabuleiro[1] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[7] == jogadorDaVez) ||
-            (tabuleiro[2] == jogadorDaVez && tabuleiro[5] == jogadorDaVez && tabuleiro[8] == jogadorDaVez)) {
+        if ((tabuleiro[0] == jogadorDaVez && tabuleiro[3] == jogadorDaVez && tabuleiro[6] == jogadorDaVez) || (tabuleiro[1] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[7] == jogadorDaVez) || (tabuleiro[2] == jogadorDaVez && tabuleiro[5] == jogadorDaVez && tabuleiro[8] == jogadorDaVez)) {
           // Se a linha tem vencedor executa
           HaVencedor = true;
-        } else if ((tabuleiro[0] == jogadorDaVez && tabuleiro[1] == jogadorDaVez && tabuleiro[2] == jogadorDaVez) ||
-                   (tabuleiro[3] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[5] == jogadorDaVez) ||
-                   (tabuleiro[6] == jogadorDaVez && tabuleiro[7] == jogadorDaVez && tabuleiro[8] == jogadorDaVez)) {
+        } else if ((tabuleiro[0] == jogadorDaVez && tabuleiro[1] == jogadorDaVez && tabuleiro[2] == jogadorDaVez) || (tabuleiro[3] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[5] == jogadorDaVez) || (tabuleiro[6] == jogadorDaVez && tabuleiro[7] == jogadorDaVez && tabuleiro[8] == jogadorDaVez)) {
           // Se a coluna tem vencedor executa
           HaVencedor = true;
 
-        } else if ((tabuleiro[0] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[8] == jogadorDaVez) ||
-                   (tabuleiro[2] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[6] == jogadorDaVez)) {
+        } else if ((tabuleiro[0] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[8] == jogadorDaVez) || (tabuleiro[2] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[6] == jogadorDaVez)) {
           // Se a diagonal tem vencedor executa
           HaVencedor = true;
 
@@ -116,26 +160,30 @@ void loop() {
             jogadorDaVez = 1;
           }
           velha = velha + 1;
-          Serial.print("Peças jogadas: "); // Imprime a qtde de jogadas
-          Serial.println(velha);
+          // Serial.print("Peças jogadas: ");  // Imprime a qtde de jogadas
+          // Serial.println(velha);
         }
       } else {
         // se for falso ...
-        Serial.println("Casa ocupada ..");
+        // Serial.println("Casa ocupada ...");
+        PartidaJSON(F("Casa ocupada ..."));
       }
     } else {
-      Serial.println("Jogada inválida!!!");
+      // Serial.println("Jogada inválida!!!");
+      PartidaJSON(F("Jogada inválida"));
     }
     // delay(1000000);
   } while (!HaVencedor && velha < 9);
 
   if (HaVencedor) {
     // Escreve Há vencedor
-    Serial.print("Jogador ");
-    Serial.print(jogadorDaVez);
-    Serial.println(" venceu!!!");
+    // Serial.print("Jogador ");
+    // Serial.print(jogadorDaVez);
+    // Serial.println(" venceu!!!");
+    PartidaJSON("Jogador " + String(jogadorDaVez) + " venceu!!!");
   } else {
     // Escreve VELHA!!!
-    Serial.println("VELHA!!!");
+    // Serial.println("VELHA!!!");
+    PartidaJSON(F("VELHA!!!"));
   }
 }
